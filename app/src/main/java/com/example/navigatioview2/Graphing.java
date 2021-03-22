7445a22b66fa5913fd54741d6b04ec9f5fdbd56b
theinsideshine <educacion.ta@gmail.com>
2021-03-22 16:35:17 -0300

    putDoubleExtra

--- app/src/main/java/com/example/navigatioview2/Graphing.java
+++ app/src/main/java/com/example/navigatioview2/Graphing.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -11,34 +12,39 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Graphing extends AppCompatActivity {
 
-
-    LineGraphSeries<DataPoint> series;
-
+    private double x=5,b,m,y;
+    private LineGraphSeries<DataPoint> series;
+    private TextView tv1,tv2,tv3,tv4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_graphing );
+        tv1 =findViewById( R.id.textView );
+        tv2 =findViewById( R.id.textView2);
+        tv3 =findViewById( R.id.textView3);
+        tv4 =findViewById( R.id.textView4);
 
         GraphView graph = (GraphView) findViewById( R.id.graph );
 
-        GraphicData data = new GraphicData();
+        Bundle bun_b= getIntent().getExtras();
+        b = bun_b.getDouble( "b" );
 
-        data.calculateTwoPoint( 0,0,1,1 );
-
-        double valueX = data.getX();  // variable x comienzo de impresion
-        double valueB = data.getB();   // ordenada en el origen
-        double valueM = data.getM();   //pendiente
-        double valueY =0 ;             // variable y
+        m=3; b=2;
+        tv1.setText("m= "+ (int) m );
+        tv2.setText("x= "+ (int) x );
+        tv3.setText("b= "+ (int) b );
 
 
 
         series = new LineGraphSeries<DataPoint>();
         for (int i = 0; i < 500; i++){
-            valueX = valueX +0.1;
-            valueY= valueM * valueX + valueB;
-            series.appendData(new DataPoint( valueX, valueY ),true,500);
+            x = x +0.1;
+            y= m * x + b;
+            series.appendData(new DataPoint( x, y ),true,500);
         }
         graph.addSeries( series );
     }
 }
+
+
